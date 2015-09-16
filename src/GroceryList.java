5781f461922c15083d91a63c969e2e29e5e50564
Sugarcoder <mikichea@gmail.com>
2015-09-15 23:17:36 -0700

    ArrayList exercise 2 - creating a grocery list

---
+++ src/GroceryList.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Sugarcoder on 9/15/2015.
+ */
+
+
+public class GroceryList {
+
+    // public int[] myNumbers = new int[50];
+
+    private ArrayList<String> groceryList = new ArrayList<String>();
+
+    public void addGroceryItems(String item) {
+        groceryList.add(item);
+    }
+
+    public void printGroceryList() {
+        System.out.println("You have " + groceryList.size() + " items in your grocery list.");
+        for(int i=0; i<groceryList.size(); i++) {
+            System.out.println((i+1) + groceryList.get(i));
+        }
+    }
+
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+        // Milk
+        // cheese
+        // Bread
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Sugarcoder on 9/15/2015.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
